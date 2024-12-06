1a20414f241f5ca7d3dbf8d8403169d25401a20d
alladinscarpet <dmw3ndwa@gmail.com>
2024-12-06 13:03:05 +0300

    Sorted verb recognizer

--- Compiler Construction/assignment1/lex.yy.c
+++ Compiler Construction/assignment1/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,27 +360,31 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[44] =
     {   0,
-        0,    0,    5,    2,    3,    1,    1,    0
+        0,    0,    4,    2,    2,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    1,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        2,    2,    2,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    4,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
-        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    5,    1,    1,    6,    7,    8,    9,
 
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+       10,    1,   11,   12,   13,    1,    1,   14,   15,   16,
+       17,    1,    1,   18,   19,    1,   20,   21,   22,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +401,59 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[23] =
     {   0,
-        1,    1,    2
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[45] =
     {   0,
-        0,    0,    5,    6,    6,    0,    0,    6,    2
+        0,    4,   69,   70,    4,    3,   58,    2,    7,   50,
+       60,   46,   52,   21,   26,   53,   37,   46,   41,   51,
+       12,   23,   42,   39,   39,   42,   35,   70,   38,   37,
+       38,   32,   39,   28,   36,   31,   29,   26,   27,   21,
+       24,    5,   70,    0
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[45] =
     {   0,
-        8,    1,    8,    8,    8,    9,    9,    0,    8
+       44,   44,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       17,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43,    0,   43
     } ;
 
-static const flex_int16_t yy_nxt[10] =
+static const flex_int16_t yy_nxt[93] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    5,   43,    5,    5,    5,   18,    5,    6,
+        7,    8,    9,   15,   10,   11,   12,   15,   19,   20,
+       16,   32,   13,   21,   43,   14,   24,   28,   28,   28,
+       25,   15,   15,   26,   42,   15,   15,   27,   28,   28,
+       28,   15,   41,   33,   15,   15,   29,   40,   15,   30,
+       15,   39,   38,   15,   37,   36,   35,   15,   34,   15,
+       31,   15,   15,   23,   15,   22,   15,   17,   43,    3,
+       43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43
+
     } ;
 
-static const flex_int16_t yy_chk[10] =
+static const flex_int16_t yy_chk[93] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+       44,    1,    1,    0,    1,    2,    2,    8,    2,    5,
+        5,    5,    5,   42,    5,    5,    5,    6,    8,    9,
+        6,   21,    5,    9,   21,    5,   14,   15,   15,   15,
+       14,   22,   41,   14,   40,   39,   38,   14,   17,   17,
+       17,   22,   37,   22,   36,   35,   17,   34,   33,   17,
+       32,   31,   30,   29,   27,   26,   25,   24,   23,   20,
+       19,   18,   16,   13,   12,   11,   10,    7,    3,   43,
+       43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
+       43,   43
+
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,13 +470,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "wordcounter.l"
-#line 2 "wordcounter.l"
+#line 1 "verbrecognizer.l"
+#line 2 "verbrecognizer.l"
 #include <stdio.h>
-int word_count = 0;
-int total_size = 0;
-#line 445 "lex.yy.c"
-#line 446 "lex.yy.c"
+#line 477 "lex.yy.c"
+#line 478 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -659,9 +691,10 @@ YY_DECL
 		}
 
 	{
-#line 7 "wordcounter.l"
+#line 5 "verbrecognizer.l"
 
-#line 665 "lex.yy.c"
+
+#line 698 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -688,13 +721,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 44 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 70 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -719,27 +752,25 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 8 "wordcounter.l"
-{ word_count++; total_size += yyleng; }  // Matches words
+#line 7 "verbrecognizer.l"
+{
+    printf("Verb: %s\n", yytext);
+}
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 9 "wordcounter.l"
-;  // Ignore other characters
+#line 11 "verbrecognizer.l"
+{ /* ignore other characters and newlines */ }
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 10 "wordcounter.l"
-;  // Ignore newlines
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 11 "wordcounter.l"
+#line 13 "verbrecognizer.l"
 ECHO;
 	YY_BREAK
-#line 743 "lex.yy.c"
+#line 774 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1036,7 +1067,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 44 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1064,11 +1095,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 44 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 43);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1744,18 +1775,16 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 11 "wordcounter.l"
+#line 13 "verbrecognizer.l"
 
 
 int main() {
-    yylex();  // Start the lexer
-    printf("Total words: %d\n", word_count);
-    printf("Total characters: %d \n", total_size);
+    yylex();  // Start lexical analysis
     return 0;
 }
 
 int yywrap() {
-    return 1;
+    return 1;  // End of input
 }
 
 
